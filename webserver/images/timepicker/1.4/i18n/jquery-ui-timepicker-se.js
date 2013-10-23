15dc27cccdd596ef26bb0c0828299ba204b66435
robe070 <goodridge.rob@gmail.com>
2013-10-23 17:27:13 +1100

    changed file endings and new files for 13.2

---
+++ webserver/images/timepicker/1.4/i18n/jquery-ui-timepicker-se.js
@@ -0,0 +1,20 @@
+﻿/* Swedish initialization for the jQuery UI timepicker plugin. */
+(function($) {
+	$.timepicker.regional['se'] = {
+		timeOnlyTitle: 'Välj tid',
+		timeText: 'Tid',
+		hourText: 'Timme',
+		minuteText: 'Minut',
+		secondText: 'Sekund',
+		millisecText: 'Millisekund',
+		microsecText: 'Mikrosekund',
+		timezoneText: 'Tidszon',
+		currentText: 'Nu',
+		closeText: 'Stäng',
+		timeFormat: 'HH:mm',
+		amNames: ['AM', 'A'],
+		pmNames: ['PM', 'P'],
+		isRTL: false
+	};
+	$.timepicker.setDefaults($.timepicker.regional['se']);
+})(jQuery);
