9c90101dd4c7d60765dd736fc858c9eea1ab5ef1
robe070 <goodridge.rob@gmail.com>
2013-10-25 08:24:00 +1100

    A commit

---
+++ pushcloud.sh
@@ -0,0 +1,11 @@
+#/bin/sh
+# Presumes that the current branch is the one to be updated
+# typically that is master
+# Requires that ssh key does NOT have a passphrase. This is because the
+# passphrase MUST be entered interactively, but this is NOT run interactively!
+echo on
+# Add all untracked files
+git add .
+# TODO Need to get comment from file
+git commit -a -m"A commit"
+git push origin master
