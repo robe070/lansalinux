2c97f457a0205575295458ef612dc6dd0efa9fe8
robe070 <goodridge.rob@gmail.com>
2013-10-25 08:27:30 +1100

    A commit

--- pushcloud.sh
+++ pushcloud.sh
@@ -1,4 +1,4 @@
-#/bin/sh -x
+#/bin/bash -x
 # Presumes that the current branch is the one to be updated
 # typically that is master
 # Requires that ssh key does NOT have a passphrase. This is because the
