df50e96f5de78cfaef622f76343e777e0fe7c724
robe070 <goodridge.rob@gmail.com>
2013-10-25 09:23:47 +1100

    Commit message from file with a newline in it

--- pushcloud.sh
+++ pushcloud.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash
 # Presumes that the current branch is the one to be updated
 # typically that is master
 # Requires that ssh key does NOT have a passphrase. This is because the
